dc5f7ddbf415043f669c84e5f1d0747c36da11ed
Unknown <brunossj5@hotmail.com>
2018-06-07 02:53:34 +0100

    Finalmente vai começar a ser mais rapido

---
+++ Assets/LowPolyNature/Scripts/Sheep.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Sheep : MonoBehaviour
+{
+    private NavMeshAgent mAgent;
+
+    private Animator mAnimator;
+
+    public GameObject Player;
+
+    public float EnemyDistanceRun = 4.0f;
+
+    private bool mIsDead = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        mAgent = GetComponent<NavMeshAgent>();
+
+        mAnimator = GetComponent<Animator>();
+    }
+
+    private bool IsNavMeshMoving
+    {
+        get
+        { 
+            return mAgent.velocity.magnitude > 0.1f;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        InteractableItemBase item = collision.collider.gameObject.GetComponent<InteractableItemBase>();
+        if(item != null)
+        {
+            if(item.ItemType == EItemType.Weapon)
+            {
+                if(Player.GetComponent<PlayerController>().IsAttacking)
+                {
+                    mIsDead = true;
+
+                    mAgent.enabled = false;
+                    mAnimator.SetTrigger("die");
+                    Destroy(GetComponent<Rigidbody>());
+                }
+            }
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: Only runaway if player is armed
+        if (mIsDead)
+            return;
+
+        bool isPlayerArmed = Player.GetComponent<PlayerController>().IsArmed;
+
+        // Performance optimization: Thx to kyl3r123 :-)
+        float squaredDist = (transform.position - Player.transform.position).sqrMagnitude;
+        float EnemyDistanceRunSqrt = EnemyDistanceRun * EnemyDistanceRun;
+
+        // Run away from player
+        if (squaredDist < EnemyDistanceRunSqrt && isPlayerArmed)
+        {
+            // Vector player to me
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position + dirToPlayer;
+
+            mAgent.SetDestination(newPos);
+
+        }
+
+        mAnimator.SetBool("walk", IsNavMeshMoving);
+
+    }
+}
---
+++ Assets/Menus/MoveScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class MoveScene : MonoBehaviour {
+
+    public Text texto;
+    public VideoPlayer vip;
+	// Use this for initialization
+	void Start () {
+        texto.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("CenaJogo");
+        }
+        if (vip.isPlaying == false)
+        {
+            texto.enabled = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("CenaJogo");
+            }
+        }
+        
+	}
+
+}
