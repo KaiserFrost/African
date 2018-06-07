9e37c2a0784a9192e0c4ebc619cc3672046b1690
Unknown <carlos-.-alves@hotmail.com>
2018-06-07 19:50:16 +0100

    Interior da Cabana do boss e boomerang a funcionar..

---
+++ Assets/Script/OnTriggerLoadLevel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTriggerLoadLevel : MonoBehaviour {
+
+    public GameObject guiObj;
+    public string lvlToLoad;
+
+	// Use this for initialization
+	void Start () {
+        guiObj.SetActive(false);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            guiObj.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("CasaPrincipal");
+
+            }
+
+        }
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            guiObj.SetActive(true);
+            if ( Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("CasaPrincipal");
+                
+            }
+
+        }
+
+        
+    }
+
+    void OnTriggerExit()
+    {
+        guiObj.SetActive(false);
+    }
+}
