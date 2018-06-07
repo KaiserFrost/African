fa5ecd9609469ce7b938f06fbfbeea5070649213
Unknown <brunossj5@hotmail.com>
2018-06-07 03:22:22 +0100

    mais um bocado

---
+++ Assets/PREFABSS/Boomerang/Boolmerang.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boolmerang : MonoBehaviour {
+
+    bool go;
+
+    GameObject player;
+    GameObject Boomerang;
+
+    Transform itemToRotate;
+
+    Vector3 posFrentePlayer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        go = false; 
+
+        //Fiz com tags pa ser mais facil
+        player = GameObject.FindGameObjectWithTag("Player");
+        Boomerang = GameObject.FindGameObjectWithTag("Boom");
+
+        Boomerang.GetComponent<MeshRenderer>().enabled = false; 
+        itemToRotate = gameObject.transform.GetChild(0);       
+
+        //Se for preciso controlar a altura ou até mesmo a posiçao em si, mas acho q ta bom assim
+        posFrentePlayer = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z) + player.transform.forward * 15f;
+        
+        StartCoroutine(Boom());
+    }
+
+    IEnumerator Boom()
+    {
+        go = true;
+        yield return new WaitForSeconds(1.5f);
+        go = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            itemToRotate.transform.Rotate(0, Time.deltaTime * 600, 0); //Roda o boomerang
+         // haver se ele manda apenas um...
+            if (go)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, posFrentePlayer, Time.deltaTime * 40); //muda a poosiçao pa frente do player           
+            }
+
+            if (!go)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z), Time.deltaTime * 40); //Volta po dono
+            }
+
+            if (!go && Vector3.Distance(player.transform.position, transform.position) < 1.5)
+            {
+                Boomerang.GetComponent<MeshRenderer>().enabled = true;
+                Destroy(this.gameObject);
+            }
+        
+        
+    }
+}
---
+++ Assets/PREFABSS/Boomerang/TesteBoomer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteBoomer : MonoBehaviour {
+
+    public GameObject boomer;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            GameObject clone;
+            clone = Instantiate(boomer, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.rotation) as GameObject;
+            //if (clone.transform.position) ;
+        }
+    }
+}
